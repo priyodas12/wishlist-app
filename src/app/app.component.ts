5b29dbb8ae315c68eaa99e8ef248b609eccf0e20
Priyobrato.Das <priyodas12@gmail.com>
2024-10-27 17:40:04 +0530

    with filter results

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,16 +7,44 @@ import WishItem from '../shared/model/wishItem';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  toggleWishList(e: WishItem) {
-    console.log('toggle clicked!', e);
-    e.isComplete = !e.isComplete;
-    console.log('post toggle', e);
-  }
   title = 'wishlist-app';
+  newWishItemText!: string;
+  selectStatus: string = '0';
 
   wishlist: WishItem[] = [
     new WishItem('Learn Typescript', true, new Date()),
     new WishItem('Learn Node', false, new Date()),
     new WishItem('Learn Angular', false, new Date()),
   ];
+
+  visibleWishlist: WishItem[] = this.getVisibleWishlist(this.selectStatus);
+
+  getVisibleWishlist(value: string): WishItem[] {
+    console.log('Changing Selection:', value);
+    if (value === '1') {
+      return this.wishlist.filter((wi) => !wi.isComplete);
+    } else if (value === '2') {
+      return this.wishlist.filter((wi) => wi.isComplete);
+    } else {
+      return this.wishlist;
+    }
+  }
+
+  addAnotherWishItem(wishText: String) {
+    console.log('Adding Another WishItem!', wishText);
+    let wishItem = new WishItem(wishText, false, new Date());
+    this.wishlist.unshift(wishItem);
+    this.visibleWishlist = this.getVisibleWishlist(this.selectStatus);
+  }
+
+  toggleWishList(e: WishItem) {
+    console.log('toggle clicked!', e);
+    e.isComplete = !e.isComplete;
+    console.log('post toggle', e);
+  }
+
+  filterChanged(value: string) {
+    console.log('Changing selection', value);
+    this.visibleWishlist = this.getVisibleWishlist(value);
+  }
 }
